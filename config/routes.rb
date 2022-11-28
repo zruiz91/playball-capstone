206a9f7b797cf8ab0ef369a1202b42144c40d04b
Zach Ruiz <powersbooth10@gmail.com>
2022-11-28 14:04:01 -0600

    uncommented in routes and controllers code for deployment

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
-  # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   get '/hello', to: 'application#hello_world'
 end
