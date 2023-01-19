6f836045bf7b7fccb3e7e99508155927cfb599a7
Zach Ruiz <powersbooth10@gmail.com>
2023-01-19 14:28:15 -0600

    added a demo in the read me

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,10 @@
-
+import ParksContainer from "./ParksContainer";
 
 const Home = () => {
     return (
         <div class="page">
             <h1>Lets Play With Your Balls</h1>
+            <ParksContainer />
         </div>
     )
 }
